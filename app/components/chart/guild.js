028f89dc0d725214ab020da6fbb5d34713954569
LeopoldHock <leopoldhock@outlook.com>
2021-12-14 19:15:46 +0100

    implemented proper scrolling

--- app/components/chart/guild.js
+++ app/components/chart/guild.js
@@ -13,16 +13,16 @@ export default class ChartGuildComponent extends ChartBaseFrequencyComponent {
     @action initialize() {
         this.data = [];
         this.labels = [];
-        for (let i = 0; i < 30; i++) {
-            this.labels.push(this.args.data[0].name);
-            this.data.push(this.args.data[0].memberCount);
-            this.backgroundColor.push(this.config.generateRandomColor());
-        }
-        // for (let guild of this.args.data) {
-        //     this.labels.push(guild.name);
-        //     this.data.push(guild.memberCount);
+        // for (let i = 0; i < 30; i++) {
+        //     this.labels.push(this.args.data[0].name);
+        //     this.data.push(this.args.data[0].memberCount);
         //     this.backgroundColor.push(this.config.generateRandomColor());
         // }
+        for (let guild of this.args.data) {
+            this.labels.push(guild.name);
+            this.data.push(guild.memberCount);
+            this.backgroundColor.push(this.config.generateRandomColor());
+        }
         super.initialize();
     }
 }
