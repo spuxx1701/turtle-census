452d5d53bbdbe66a182eb47ba2eadbe173a997d4
LeopoldHock <leopoldhock@outlook.com>
2022-01-24 10:05:29 +0100

    implemented loading indicator

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,18 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
-export default class ClassCompositionRoute extends Route {
+export default class ApplicationRoute extends Route {
     @service dataLoader;
 
     async model() {
         this.dataLoader.load();
+    }
+
+    @action
+    loading() {
+        //   let controller = this.controllerFor('foo');
+        //   controller.set('currentlyLoading', true);
+        return true; // allows the loading template to be shown
     }
 }
